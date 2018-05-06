3a7820879860ca1e74bccf9074020e17b0e7678d
liqiqi <liqiqi@1076.com>
2018-05-06 19:18:51 +0800

    项目结构完成

---
+++ src/components/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div calss="box">
+      这是首页
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less" scoped>
+@color:pink;
+.box{
+  color:@color;
+}
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      这是登陆页
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
